3ec7e57b640df62b697a847c92e4986b95d29930
NicholasKhatch <nicholas.khatchadourian23@ycdsbk12.ca>
2022-01-06 18:23:13 -0500

    test

--- Sketch.java
+++ Sketch.java
@@ -16,20 +16,21 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    background(204, 255, 249);
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+    background(204, 255, 249);
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    fill(0,0,0);
+    ellipse(mouseX,mouseY,20,20);
+
+    stroke(0, 0, 0);
+    fill(63, 122, 55);
+    rect(0,350,400,50);
   }
   
   // define other methods down here.
